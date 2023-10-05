3aad83f03c383fe2d56f241f1b0d6638e2840607
YUSIEUN1013 <db4067ww@gmail.com>
2023-10-05 10:31:05 +0900

    Initial commit

---
+++ src/main/java/com/example/ui_demo/UiDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.ui_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiDemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(UiDemoApplication.class, args);
+    }
+
+}
